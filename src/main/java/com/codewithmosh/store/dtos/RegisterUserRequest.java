73a978aab6850b7c067b3d4e08688eb46d6c52db
Anatole HAGBE <ahagbes89@gmail.com>
2026-01-14 22:20:21 +0100

    feat: Add global exception handler

--- src/main/java/com/codewithmosh/store/dtos/RegisterUserRequest.java
+++ src/main/java/com/codewithmosh/store/dtos/RegisterUserRequest.java
@@ -1,10 +1,20 @@
 package com.codewithmosh.store.dtos;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class RegisterUserRequest {
+    @NotBlank(message = "Name is required")
+    @Size(min = 3, max = 255, message = "Name must be between 3 and 255 characters")
     private String name;
+
+    @Email(message = "Invalid email")
     private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters")
     private String password;
 }
